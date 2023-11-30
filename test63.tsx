a65e79ef1c1ad85133569a693aa784667d028d6c
hding49 <hding49@uwo.ca>
2023-11-30 16:29:12 -0500

    test

--- test63.tsx
+++ test63.tsx
@@ -5,3 +5,12 @@ Node.js acts as a proxy server, connecting the frontend to the backend services,
 The Model layer manages data persistence and retrieval, interacting with the database through an ORM or other data access mechanisms. Views are implemented using React components, styled and structured with Bootstrap for responsive and user-friendly interfaces. Controllers handle user inputs, orchestrating interactions between views and models, ensuring a seamless flow of data and actions.
 
 This design fosters a scalable, maintainable, and modular codebase, promoting separation of concerns and facilitating development, testing, and future enhancements.
+
+
+
+
+Landing Page:
+The Landing Page consists of various components organized using React Router, showcasing essential information or functionalities to users. It serves as the initial interface, providing an overview or introductory content about the application's core features or services. Components such as headers, hero sections, or feature highlights are seamlessly navigable using React Router's routing capabilities, creating a smooth and engaging user experience.
+
+Search Page:
+The Search Page is a collection of React Router-managed components designed to facilitate user searches efficiently. It offers a dedicated space where users can input search queries and view search results through components like search bars, result displays, and filtering options. React Router enables seamless transitions between different search-related components, enhancing usability and accessibility for users exploring and interacting with search functionalities.
